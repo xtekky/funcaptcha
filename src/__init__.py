2c9e22cdabd9e2ade7eef919a6888d3731cfca58
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-10-11 05:53:05 +0800

    Two functions has been optimized.
    Two functions has been optimized. Will optimize the other ones soon.

--- src/__init__.py
+++ src/__init__.py
@@ -43,13 +43,11 @@ class Funcaptcha:
     def _decrypt(data, key):
         data = json.loads(data)
         dk = key.encode() + bytes.fromhex(data["s"])
-
-        md5 = [hashlib.md5(dk).digest()]
-        result = md5[0]
-        for i in range(1, 3 + 1):
-            md5.insert(i, hashlib.md5((md5[i - 1] + dk)).digest())
-            result += md5[i]
-
+        result = reduce(
+            lambda x, y: x + y,
+            [hashlib.md5(dk).digest()] +
+            [hashlib.md5((md5[i - 1] + dk)).digest() for i in range(1, 3 + 1)]
+        )
         aes = AES.new(result[:32], AES.MODE_CBC, bytes.fromhex(data["iv"]))
         data = aes.decrypt(base64.b64decode(data["ct"]))
         return data
@@ -59,29 +57,49 @@ class Funcaptcha:
         timeframe = int(ts - (ts % 21600))
         key = self.user_agent + str(timeframe)
 
-        data = []
-        data.append({"key": "api_type", "value": "js"})
-        data.append({"key": "p", "value": 1})
-
         fonts = "Arial,Arial Black,Arial Narrow,Book Antiqua,Bookman Old Style,Calibri,Cambria,Cambria Math,Century,Century Gothic,Century Schoolbook,Comic Sans MS,Consolas,Courier,Courier New,Garamond,Georgia,Helvetica,Impact,Lucida Bright,Lucida Calligraphy,Lucida Console,Lucida Fax,Lucida Handwriting,Lucida Sans,Lucida Sans Typewriter,Lucida Sans Unicode,Microsoft Sans Serif,Monotype Corsiva,MS Gothic,MS PGothic,MS Reference Sans Serif,MS Sans Serif,MS Serif,Palatino Linotype,Segoe Print,Segoe Script,Segoe UI,Segoe UI Light,Segoe UI Semibold,Segoe UI Symbol,Tahoma,Times,Times New Roman,Trebuchet MS,Verdana,Wingdings,Wingdings 2,Wingdings 3".split(",")
         plugins = "Chrome PDF Plugin,Chrome PDF Viewer,Native Client".split(",")
         canvas_fp = -1424337346
 
-        fe = [
-            "DNT:unknown", "L:en-US", "D:24", "PR:1", "S:1920,1080", "AS:1920,1040", "TO:-120", "SS:true", "LS:true", "IDB:true", "B:false", "ODB:true", "CPUC:unknown", "PK:Win32", f"CFP:{str(canvas_fp)}", "FR:false", "FOS:false", "FB:false", f"JSF:{', '.join(fonts)}", f"P:{', '.join(plugins)}", "T:0,false,false", "H:8", "SWF:false"
-        ]
+        data = {
+            "key": ["api_type", "p"],
+            "value": ["js", 1]
+        }
 
-        fp = secrets.token_hex(16)
-        ife_hash = self.pyjs.call("x64hash128", ", ".join(fe), 38)
-        wh = secrets.token_hex(16) + "|" + secrets.token_hex(16)
-
-        data.append({"key": "f", "value": fp})
-        data.append({"key": "n", "value": base64.b64encode(str(int(ts)).encode("utf-8")).decode("utf-8")})
-        data.append({"key": "wh", "value": wh})
-        data.append({"key": "fe", "value": fe})
-        data.append({"key": "ife_hash", "value": ife_hash})
-        data.append({"key": "cs", "value": 1})
-        data.append({"key": "jsbd", "value": '{"HL":28,"NCE":true,"DA":null,"DR":null,"DMT":31,"DO":null,"DOT":31}'})
+        data["key"].extend(["f", "n", "wh", "fe", "ife_hash", "cs", "jsbd"])
+        data["value"].extend([
+            secrets.token_hex(16),
+            base64.b64encode(str(int(ts)).encode("utf-8")).decode("utf-8"),
+            secrets.token_hex(16) + "|" + secrets.token_hex(16),
+            [
+                "DNT:unknown",
+                "L:en-US",
+                "D:24",
+                "PR:1",
+                "S:1920,1080",
+                "AS:1920,1040",
+                "TO:-120",
+                "SS:true",
+                "LS:true",
+                "IDB:true",
+                "B:false",
+                "ODB:true",
+                "CPUC:unknown",
+                "PK:Win32",
+                f"CFP:{str(canvas_fp)}",
+                "FR:false",
+                "FOS:false",
+                "FB:false",
+                f"JSF:{', '.join(fonts)}",
+                f"P:{', '.join(plugins)}",
+                "T:0,false,false",
+                "H:8",
+                "SWF:false"
+            ],
+            self.pyjs.call("x64hash128", ", ".join(data["value"][4]), 38),
+            1,
+            '{"HL":28,"NCE":true,"DA":null,"DR":null,"DMT":31,"DO":null,"DOT":31}'
+        ])
 
         data = json.dumps(data, separators=(',', ':'))
         data = Funcaptcha._encrypt(data, key)
