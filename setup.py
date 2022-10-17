7ececc66f6352c8c1e15729c9c09a52576400a30
Adi Nicolae <nicolae.adrian.m@gmail.com>
2022-10-17 03:49:14 +0300

    Keep it clean-err!

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Education',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
-    ]
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
 
 setup(
     name                 = 'funcaptcha',
@@ -19,4 +19,4 @@ setup(
     license              = 'MIT',
     install_requires     = ['requests', 'certifi', 'idna', 'Pillow', 'pycryptodome', 'PyExecJS', 'urllib3', 'charset-normalizer', 'six'],
     include_package_data = True
-    )
+)
